ce3affd67c99517d5704370f9b22452c05dfaca5
imark4 <imark4@daywrkw7313.enterprisenet.org>
2017-11-23 00:05:13 -0800

    hello

---
+++ AutomationClaritas/src/main/java/ClaritasAutomation/AutomationClaritas/App.java
@@ -0,0 +1,13 @@
+package ClaritasAutomation.AutomationClaritas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
