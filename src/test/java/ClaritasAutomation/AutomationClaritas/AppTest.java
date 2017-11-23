60d5baf8e4a52cc64cae82e0ed882665287b091f
Claritas360 <33858053+Claritas360@users.noreply.github.com>
2017-11-23 00:11:45 -0800

    Add files via upload

---
+++ src/test/java/ClaritasAutomation/AutomationClaritas/AppTest.java
@@ -0,0 +1,31 @@
+package ClaritasAutomation.AutomationClaritas;
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+
+@Test
+
+public class AppTest {
+	
+	
+	public void testProgram(){
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver.exe");
+		WebDriver driver= new FirefoxDriver();
+		driver.manage().window().maximize();
+		System.out.println("Hi");
+		driver.get("https://www.ultimatix.net");
+		driver.close();
+		
+		
+		
+	}
+	
+	
+   
+}
